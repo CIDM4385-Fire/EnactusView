c8d683835ffd56c32f190cd7766bfcd2c125ecab
BryanHill8 <bryanhill_7@yahoo.com>
2015-03-15 19:10:41 -0500

    Fixed the login view and I am also working on implementing pictures.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,25 +1,7 @@
-var win = Ti.UI.createWindow({
+/*var win = Ti.UI.createWindow({
 	backgroundColor : "black"
 });
-var emailTextField = Ti.UI.createTextField({
-  borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-  color: '#FFFFFF',
-  top: "25%", 
-  hintText:"Email:",
-  width: 300, height: 60
-});
-win.add(emailTextField);
-win.open();
-
-var passwordTextField = Ti.UI.createTextField({
-	borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
-	color: '#FFFFFF',
-	top: "45%",
-	hintText:"Password",
-	width: 300, height: 60
-});
-win.add(passwordTextField);
-win.open();
+*/
 
 function doClick(e){	
   alert("You are logged in");
